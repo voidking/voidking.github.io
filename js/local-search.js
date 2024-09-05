8a7fa5933fd3bcbc7d85be17b2953cc9e92fa810
voidking <voidking@qq.com>
2024-09-05 12:11:19 +0000

    GitHub Actions Auto Builder

--- js/local-search.js
+++ js/local-search.js
@@ -15,7 +15,7 @@ window.addEventListener('DOMContentLoaded', () => {
   let path = CONFIG.root + searchPath;
   // Use jsDelivr CDN to accelerate the speed of loading search.xml
   if ('cdn' in CONFIG.localsearch && CONFIG.localsearch.cdn.enable === true && CONFIG.localsearch.cdn.url !== null){
-    t = '202408030449';
+    t = '202409051209';
     path = CONFIG.localsearch.cdn.url + '?t=' + t;
   }
 
